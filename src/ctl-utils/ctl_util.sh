b8713e8583adb37ebb4375be5e8022b69a00ca5e
Joachim Valdez <valdezj@us.ibm.com>
2017-05-08 15:48:28 -0400

    Export monit functions to ctl-utils package
    [#144596281]
    
    Signed-off-by: Angela Chin <achin@pivotal.io>

--- src/ctl-utils/ctl_util.sh
+++ src/ctl-utils/ctl_util.sh
@@ -17,3 +17,54 @@ function wait_for_server_to_become_healthy() {
   echo 1
 }
 
+function handle_orphaned_server() {
+  local job_name=$1
+  local pid=$2
+  echo "killing ${job_name} with pid ${pid}"
+  kill "${pid}"
+}
+
+function wait_pid() {
+  local pid=$1
+  local max_checks=$2
+
+  checks=$max_checks
+  while [ -e "/proc/$pid" ]; do
+    checks=$((checks - 1))
+    if [ $checks -le 0 ]; then
+      return 1
+    fi
+    sleep 0.1
+  done
+
+  return 0
+}
+
+function stop_process() {
+  local pid
+  pid=$(cat "$PIDFILE")
+
+  echo "stopping..."
+  kill -TERM "$pid"
+  if wait_pid "$pid" 100 ; then
+    rm "$PIDFILE"
+    return 0
+  fi
+
+  echo "unable to stop process using SIGTERM after 10 seconds, will now attempt to SIGQUIT"
+  kill -QUIT "$pid" || true
+  if wait_pid "$pid" 50 ; then
+    rm "$PIDFILE"
+    return 0
+  fi
+
+  echo "unable to stop process using SIGQUIT after 5 seconds, will now attempt to SIGKILL"
+  kill -KILL "$pid" || true
+  if wait_pid "$pid" 50 ; then
+    rm "$PIDFILE"
+    return 0
+  fi
+
+  echo "unable to stop process using SIGKILL after 5 seconds"
+  return 1
+}
