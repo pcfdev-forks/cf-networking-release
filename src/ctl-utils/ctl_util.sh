2356dec5724a54259c76eccaf72dbe4e6afd4983
David McClure <dmcclure@pivotal.io>
2017-05-08 09:39:03 -0700

    WIP
    [#144596281]
    
    Signed-off-by: Angela Chin <achin@pivotal.io>

---
+++ src/ctl-utils/ctl_util.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+function wait_for_server_to_become_healthy() {
+  local url=$1
+  local timeout=$2
+  for i in $(seq $timeout); do
+    set +e
+    curl -f --connect-timeout 1 $url > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      echo 0
+      return
+    fi
+    set -e
+    sleep 1
+  done
+
+  echo 1
+}
+
